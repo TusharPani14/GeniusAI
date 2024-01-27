25e68bda55a1e2a9e8520832bf2fb4caebaf570d
Tushar Panigrahi <tusharpanigrahi66@gmail.com>
2024-01-28 02:04:22 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Genius",
   description: "AI platform",
+  icons: {
+    icon: '/logo.png', 
+  },
 };
 
 export default function RootLayout({
